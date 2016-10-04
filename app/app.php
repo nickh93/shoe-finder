e5750660dc35f3c8a2c2d30f1aa60b6f2b81ccfe
nicolas <nickh93@outlook.com>
2016-10-04 09:54:51 -0400

    added SQL database and set up for app.php index.php and the test files

---
+++ app/app.php
@@ -0,0 +1,22 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:8889;dbnmae=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    return $app;
+
+?>
---
+++ tests/ClientTest.php
@@ -0,0 +1,21 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Brand.php";
+    require_once "src/Store.php";
+
+    $server ='mysql:host=localhost:8889;dbname=shoes_test'
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class ClientTest extends PHPUnit_Framework_TestCase
+    {
+      
+    }
+
+?>
